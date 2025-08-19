73fdd160cc56131f3758071f54715c907128099d
udud0099 <udud0099@gmail.com>
2025-08-19 18:05:47 +0545

    learn basic CRUD - npm i express-session start

--- Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/index.js
+++ Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/create", async function (req, res, next) {
+  // res.render("index", { title: "Express" });
+  const createduser = await userModel.create({
+    username: "Shiddddd",
+    name: "siddant",
+    age: 99,
+  });
+  res.send(createduser);
+});
+
+router.get("/allusers", async function (req, res, next) {
+  let allusers = await userModel.find();
+  res.send(allusers);
+});
+
+router.get("/oneusers", async function (req, res, next) {
+  let oneusers = await userModel.findOne({ name: "whoamiii0" });
+  res.send(oneusers || "no user fonund");
+});
+
+router.get("/delete", async function (req, res, next) {
+  const deletedUser = await userModel.findOneAndDelete({ name: "whoamiii" });
+  res.send(deletedUser);
 });
 
 module.exports = router;
--- Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/users.js
+++ Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/users.js
@@ -13,5 +13,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/practicekaro");
 const userschema = mongoose.Schema({
   username: String,
   name: String,
-  age:Number
-})
+  age: Number,
+});
+
+module.exports = mongoose.model("user", userschema);
