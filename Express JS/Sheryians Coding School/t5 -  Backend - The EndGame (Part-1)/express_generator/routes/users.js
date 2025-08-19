00c219e8ec2eb01f37a108a7aef9ccec284c420a
udud0099 <udud0099@gmail.com>
2025-08-19 17:25:57 +0545

    start mongo db

--- Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/users.js
+++ Express JS/Sheryians Coding School/t5 -  Backend - The EndGame (Part-1)/express_generator/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/practicekaro");
+
+const userschema = mongoose.Schema({
+  username: String,
+  name: String,
+  age:Number
+})
