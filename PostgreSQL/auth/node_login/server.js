6d0fc037dbe1383235447b0627f5e679c96fa9e2
udud0099 <udud0099@gmail.com>
2025-08-28 18:09:27 +0545

    try to auth

---
+++ PostgreSQL/auth/node_login/server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+
+const { pool } = require("./dbConfig");
+
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+
+const PORT = process.env.PORT || 4000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: "secret",
+
+    resave: false,
+
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users/register", (req, res) => {
+  res.render("register");
+});
+app.get("/users/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard", { user: "ok" });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({ name, email, password, password2 });
+
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "plaassword min 6" });
+  }
+  if (password != password2) {
+    errors.push({ message: "plaassword not same" });
+  }
+  if (errors.length > 0) {
+    res.render("register", { errors });
+  } else {
+    // form validation has passed
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    pool.query(
+      `SELECT * FROM users 
+      WHERE email = $1`,
+      [email],
+      (erra, results) => {
+        if (erra) {
+          throw erra;
+        }
+        console.log("check");
+
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          errors.push({ message: "email already" });
+          res.render("register", { errors });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password) VALUES($1, $2, $3)
+            RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "you are now register plz login");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server run on port ${PORT}`);
+});
